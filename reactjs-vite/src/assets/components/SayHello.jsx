df2c72135a7fc6e0a8aa3d82d49e24c113ea4902
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-05 10:54:07 -0800

    added new props in my component

--- reactjs-vite/src/assets/components/SayHello.jsx
+++ reactjs-vite/src/assets/components/SayHello.jsx
@@ -4,6 +4,7 @@ function SayHello(props) {
   return (
     <div>
         <h1>My name is {props.name} and i'm {props.age} old</h1>
+        {props.age >=25 ? <h3>Congratulation you are eligible to this program</h3>:<h3>Sorry!Try again later</h3>}
     </div>
   )
 }
