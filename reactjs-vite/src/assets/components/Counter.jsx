55be12726d207ea90b28e243fb76f44b5e60e633
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-06 12:48:29 -0800

    added style with tailwindCSS to my component

--- reactjs-vite/src/assets/components/Counter.jsx
+++ reactjs-vite/src/assets/components/Counter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 function Counter() {
 
@@ -18,10 +18,13 @@ function Counter() {
     }
   return (
     <div>
-    <p>{count}</p>
-    <button onClick ={decrement}>Decrement</button>
-    <button onClick ={reset}>Reset</button>
-    <button onClick ={increment}>Increment</button>
+    <p className="ml-[100px] text-3xl font-bold">{count}</p>
+    <div className="flex gap-2">
+    <button className="bg-orange-400 text-sm text-center rounded-md p-2 border-none" onClick ={decrement}>Decrement</button>
+    <button className="bg-blue-500 text-white text-sm rounded-md border-none p-2" onClick ={reset}>Reset</button>
+    <button className ="bg-yellow-300 text-white tex-sm rounded-md border-none p-2" onClick ={increment}>Increment</button>
+    </div>
+    
     </div>
   )
 }
