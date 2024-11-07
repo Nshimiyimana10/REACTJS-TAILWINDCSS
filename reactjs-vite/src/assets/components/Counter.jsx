9429ac568135065b000bf458491461b88a67a30b
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-07 11:30:39 -0800

    added onchange event

--- reactjs-vite/src/assets/components/Counter.jsx
+++ reactjs-vite/src/assets/components/Counter.jsx
@@ -4,6 +4,10 @@ import {useState} from 'react';
 function Counter() {
 
     const [count, setCount] = useState(0);
+    const [name, setName] = useState();
+    const newName = (e)=>{
+        setName(e.target.value);
+    }
 
     function decrement(){
         setCount(count+1);
@@ -16,6 +20,8 @@ function Counter() {
     function increment(){
         setCount(count+1);
     }
+
+    
   return (
     <div>
     <p className="ml-[100px] text-3xl font-bold">{count}</p>
@@ -23,8 +29,10 @@ function Counter() {
     <button className="bg-orange-400 text-sm text-center rounded-md p-2 border-none" onClick ={decrement}>Decrement</button>
     <button className="bg-blue-500 text-white text-sm rounded-md border-none p-2" onClick ={reset}>Reset</button>
     <button className ="bg-yellow-300 text-white tex-sm rounded-md border-none p-2" onClick ={increment}>Increment</button>
+    <input type="text" placeholder='Enter your name' onChange={newName}/>
+    <p>updated name is: {name}</p>
     </div>
-    
+      
     </div>
   )
 }
