2e7555f4af4fa7f36218f2c32c2e0fe14bf53ec0
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-20 11:04:21 -0800

    added

--- reactjs-vite/src/assets/components/CounterFn.jsx
+++ reactjs-vite/src/assets/components/CounterFn.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react';
 
 function CounterFn() {
     const[count,setCount] = useState('0');
+    const[color, setColor] = useState("green");
     function addCount(){
         setCount((count) => count + 1)
     }
@@ -10,11 +11,15 @@ function CounterFn() {
     function removeCount(){
         setCount((count) => count - 1);
     }
+
+    useEffect(() =>{
+       document.title = `My count is ${count} ${color}`;
+    },[]);
   return (
-    <div>
+    <div className ="bg-gray-300 rounded-md w-60 h-72 flex flex-col justify-center items-center gap-3">
        <p>My count is {count}</p>
-      <button onClick={addCount}>ADD</button>
-      <button onClick={removeCount}>SUBSTRACT</button>
+      <button className="bg-blue-600 text-white text-center rounded-md border-none p-3 text-xs" onClick={addCount}>ADD</button>
+      <button  className="bg-green-600 text-white text-center rounded-md border-none p-3 text-xs"  onClick={removeCount}>SUBSTRACT</button>
     </div>
   )
 }
