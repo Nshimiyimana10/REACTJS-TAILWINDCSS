86e5ccea0c0ad88987cdb003be23c9327c250598
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-27 10:18:22 -0800

    added component

--- reactjs-vite/src/assets/components/ComponentA.jsx
+++ reactjs-vite/src/assets/components/ComponentA.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, createContext} from 'react';
+import ComponentB from './assets/components/ComponentB';
+
+const userContext =createContext();
 
 function ComponentA() {
+const [user, setUser] = useState("Peter");
   return (
-    <div>ComponentA</div>
+    <div>
+        <h2>This is useContext hook</h2>
+        <userContext.Provider value ={user}>
+            <ComponentB/>
+        </userContext.Provider>
+    </div>
   )
 }
 
